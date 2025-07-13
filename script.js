876d10ad2be4f91ea421003f5bf4d467db2720a5
alonso001270 <clicexpress.cr@gmail.com>
2025-07-13 12:08:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function hacerPedido() {
     alert("Función de pedido activada");
 }
+document.getElementById('form-pedido').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  const nombre = this.nombre.value;
+  const telefono = this.telefono.value;
+  const direccion = this.direccion.value;
+  const detalle = this.detalle.value;
+  const pago = this.pago.value;
+
+  console.log("Pedido:", { nombre, telefono, direccion, detalle, pago });
+
+  // Mostrar mensaje de éxito
+  document.getElementById("mensaje-exito").style.display = "block";
+
+  // Limpiar formulario
+  this.reset();
+});
